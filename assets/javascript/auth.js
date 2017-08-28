095facbdf6aa16797b93a17cd4fbdd36847c2e32
Erik Streff <Estreff@gmail.com>
2017-08-28 17:18:11 -0600

    test gmail branch

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -9,6 +9,25 @@ $(function(){
     messagingSenderId: "206272245401"
   };
   firebase.initializeApp(config);
+
+  // FirebaseUI config.
+      var uiConfig = {
+        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>'
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
    
       var auth = firebase.auth(); 
 
@@ -45,12 +64,6 @@ $(function(){
       // Login
       var promise = auth.signInWithEmailAndPassword(loginEmail, loginPswd);
       promise.catch(event => console.log(event.message));
-
-
-      // $('#login-nav').addClass('hide');
-      // $('#signup-nav').addClass('hide');
-      // $('#logout-nav').removeClass('hide');
-
 
     });
 
