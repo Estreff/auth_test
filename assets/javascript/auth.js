7a4b8ba7c57d654e113233ca2be3c26ef9d0cf78
Erik Streff <Estreff@gmail.com>
2017-08-29 20:17:37 -0600

    create user info in firebase

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -11,6 +11,8 @@ $(function(){
   firebase.initializeApp(config);
 
       var auth = firebase.auth(); 
+      var golfdb = firebase.database();
+      var playerId = 0;
 
   // Sign-up Navbar to show the form
 
@@ -32,19 +34,31 @@ $(function(){
     $('#signup-form').addClass('hide')
   });
 
+  var email = "";
+  var firstName = "";
+  var lastName = "";
+  var loginPswd = "";
+  var uid = "";
+  var gamesPlayed = [0];
+
   // Add Login on click
     $('#login').click(function(event){
       // Get email and password
-      var loginEmail = $('#email-login').val().trim();
-      console.log(loginEmail);
-      var loginPswd = $('#password-login').val().trim();
+      email = $('#email-login').val().trim();
+      console.log(email);
+      loginPswd = $('#password-login').val().trim();
       console.log(loginPswd);
 
       event.preventDefault();
 
       // Login
-      var promise = auth.signInWithEmailAndPassword(loginEmail, loginPswd);
+      var promise = auth.signInWithEmailAndPassword(email, loginPswd);
       promise.catch(event => console.log(event.message));
+
+      // $('#player').text('Username');
+
+      $('#email-login').val("")
+      $('#password-login').val("")
 
     });
 
@@ -52,23 +66,40 @@ $(function(){
 
     $('#createAcct').click(function(event){
       // Get email and password
-// TODO: Check for valid Email
-      var loginEmail = $('#email-input').val().trim();
-      console.log(loginEmail);
-      // var loginEmail = $('#fName-input').val().trim();
-      // console.log(loginEmail);
-      // var loginEmail = $('#lName-input').val().trim();
-      // console.log(loginEmail);
-      var loginPswd = $('#password-input').val().trim();
+      email = $('#email-input').val().trim();
+      console.log(email);
+      firstName = $('#fName-input').val().trim();
+      console.log(firstName);
+      lastName = $('#lName-input').val().trim();
+      console.log(lastName);
+      loginPswd = $('#password-input').val().trim();
       console.log(loginPswd);
-      // var loginPswd = $('#verifyPassword-input').val().trim();
-      // console.log(loginPswd);
+
+      // $('#player').text('Username');
 
       event.preventDefault();
 
+
       // Create User
-      var promise = auth.createUserWithEmailAndPassword(loginEmail, loginPswd);
+      var promise = auth.createUserWithEmailAndPassword(email, loginPswd);
       promise.catch(event => console.log(event.message));
+
+      var empsRef = golfdb.ref("users");
+
+      empsRef.child(firstName + " " + lastName).set({
+        playerId: playerId,
+        firstname: firstName,
+        lastName: lastName,
+        email: email,
+        games: gamesPlayed,
+        dataAdded: firebase.database.ServerValue.TIMESTAMP
+      })
+
+
+      $('#email-input').val("");
+      $('#fName-input').val("");
+      $('#lname-input').val("");
+      $('#password-input').val("");
 
     });
 
@@ -82,14 +113,15 @@ $(function(){
     firebase.auth().onAuthStateChanged(firebaseUser => {
       if(firebaseUser) {
         console.log('Valid User: ', firebaseUser);
-        $('#logout-nav').removeClass('hide')
+        $('#logout-nav').removeClass('hide');
         $('#login-nav').addClass('hide');
         $('#signup-nav').addClass('hide');
         $('#login-form').addClass('hide')
         $('#signup-form').addClass('hide')
+        
       } else {
         console.log('not logged in');
-        $('#logout-nav').addClass('hide')
+        $('#logout-nav').addClass('hide');
         $('#login-nav').removeClass('hide');
         $('#signup-nav').removeClass('hide');
       }
