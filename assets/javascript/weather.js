03a2e870bb64eb902961883c70edc6b1459e23b9
Brian Russell <brianrobertrussell@gmail.com>
2017-09-06 21:32:09 -0600

    improved Geolocation.

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -3,45 +3,67 @@
 
 var wxIcon; //Wx icon code
 var wxIconPath; //path to Wx icon
-var userLocation = "Denver, CO, USA" 
 var apiKey = "b1d8243e585e458550ec2db368435c82";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ userLocation + "&units=imperial&appid=" + apiKey; 
+var lat;// = 40;
+var long;// = -105;
 
-	//console.log(queryURL);
+getLocation();
 
-$.ajax({
-	url: queryURL,
-	method: "GET"
-})
-.done(function(response) {//Run this code after API get is complete
-    
-    console.log(response);
+// console.log(navigator.geolocation.getCurrentPosition());
 
-    //create path weather icon
-    wxIcon = response.weather[0].icon
+function getLocation(){  
+    if (navigator.geolocation){
+        console.log("Geolocation found");
+        navigator.geolocation.getCurrentPosition(myPosition);
+    } else {
+        console.log("Geolocation may not be available on this device");
+    };
+};
 
-    wxIconPath = "https://openweathermap.org/img/w/"+ wxIcon +".png";
+function myPosition(position){
+    console.log(position.coords)
+    lat = position.coords.latitude;
+    console.log("lat: ", lat);
+    long = position.coords.longitude
+    console.log("long: ", long);
+    runAjax();
+};
 
-    //display City
-    $("#city").text(response.name)
+//this funtion makes the ajax query
+function runAjax(){
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .done(function(response) {//Run this code after API get is complete
+        
+        console.log(response);
 
-    //Display temperature a s whole number with degree symbol
-    $("#temperature").text(Math.round(response.main.temp)+String.fromCharCode(176));
-    
-    //Add weather Icon to page
-    $("#weather-icon").html("<img src= " + wxIconPath + ">")
+        //create path weather icon
+        wxIcon = response.weather[0].icon
 
-    //textual description of weather
-    $("#weather-description").text(response.weather[0].main);
-    //display windspeed
-    $("#windspeed").text(Math.round(response.wind.speed) + "mph")
-    
-    //display wind direction arrow
-    $("#wind-direction").html("<img src= 'assets/images/windIndicator.png'>")
+        wxIconPath = "https://openweathermap.org/img/w/"+ wxIcon +".png";
 
-    //rotate arrow into the wind using 3rd party plugin
-    $("#wind-direction").rotate(response.wind.deg);
+        //display City
+        $("#city").text(response.name)
 
+        //Display temperature a s whole number with degree symbol
+        $("#temperature").text(Math.round(response.main.temp)+String.fromCharCode(176));
+        
+        //Add weather Icon to page
+        $("#weather-icon").html("<img src= " + wxIconPath + ">")
 
-}); //end ajax.done
+        //textual description of weather
+        $("#weather-description").text(response.weather[0].main);
+        //display windspeed
+        $("#windspeed").text(Math.round(response.wind.speed) + "mph")
+        
+        //display wind direction arrow
+        $("#wind-direction").html("<img src= 'assets/images/windIndicator.png'>")
+
+        //rotate arrow into the wind using 3rd party plugin
+        $("#wind-direction").rotate(response.wind.deg);
+    }); //end ajax.done
+};
